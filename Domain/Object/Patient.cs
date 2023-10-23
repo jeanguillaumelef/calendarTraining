f0b43066076bc085ed5cad61db903201f8eaaf53
jeanguillaumelef <jeanguillaumelefebvre@gmail.com>
2023-10-23 10:17:20 +0200

    added Guid to patient for future storage

--- Domain/Object/Patient.cs
+++ Domain/Object/Patient.cs
@@ -4,12 +4,14 @@ namespace Domain.Object
 {
     public class Patient
     {
+        public Guid Id { get;}
         public string Name { get; set; }
         //should be a list loaded from a DB
         public string AnimalType { get; set; } //dog cat etc..
 
         public Patient(string name, string animalType)
         {
+            Id = Guid.NewGuid();
             this.Name = name;
             this.AnimalType = animalType;
         }
